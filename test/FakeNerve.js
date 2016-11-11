3403264998dd99e9268a57b3d0f033b73bd8008a
benjaminjt <me@benjaminjt.com>
2016-11-12 05:22:32 +1100

    Fake nerve for testing

---
+++ test/FakeNerve.js
@@ -0,0 +1,27 @@
+const { Nerve } = require('../source');
+
+/**
+ * Fake nerve class for testing
+*/
+module.exports = class FakeNerve extends Nerve {
+  constructor(name, options) {
+    super(name);
+    this.options = options || {};
+    if (this.options.noInit === true) this.init = null;
+    if (this.options.noExit === true) this.exit = null;
+    if (this.options.noName === true) this.name = null;
+  }
+  fireReady() {
+    this.fire('ready');
+  }
+  fireEnd() {
+    this.fire('end');
+  }
+  init() {
+    if (this.options.initThrows) throw new Error('Fake nerve init error');
+    if (this.options.waitForReady !== true) this.fire('ready');
+  }
+  exit() {
+    if (this.options.waitForEnd !== true) this.fire('end');
+  }
+};
